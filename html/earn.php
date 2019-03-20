879ced6ee78820c369c6e551e88184c36cee775a
timmehz <34901910+timmehz@users.noreply.github.com>
2019-03-19 19:54:54 -0600

    Add files via upload

---
+++ html/earn.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+	
+<head>
+	<link rel="shortcut icon" href="imgs/Steam_Rewards_Logo.png" />
+	<title>Steam Rewards-Home</title>
+	<link rel = "stylesheet" href = "css/welcome_website.css">
+</head>
+
+<body>
+	<center><h1 class = "welcome_message">Welcome to Steam Rewards Offical Website!</h1></center>
+</body>
+
+</html>
+<!doctype html>
+<html lang="en">
+   <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+      <!-- Custom CSS -->
+      <link rel="stylesheet" href="css/style.css">
+      <title>CS 30 - HTML & Bootstrap</title>
+   </head>
+   <body>
+      <!-- Navigation -->
+      <header>
+         <!-- Fixed Navigation -->
+         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <a class="navbar-brand" href="#" >Steam Rewards</a>
+            <!-- Mobile Hamburger Nav -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+               <ul class="navbar-nav mr-auto">
+                  <li class="nav-item active">
+                     <a class="nav-link" href="index.html">Home</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="earn.php">Earn</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="discord.php">Discord</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="data.php">Login/Signup</a>
+                  </li>
+               </ul>
+            </div>
+         </nav>
+      </header>
+      <section>
+         <!-- Begin Page Content -->
+         <div class="container-fluid" style="padding-top: 100px;>
+            <div class="row">
+            <div class="col" align="center">
+               <!-- Content Here -->
+            </div>
+         </div>
+      </section>
+      <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+      <footer class="footerstyle">
+         <!-- Footer -->
+         <div class="container">
+            Copyright 2019 :: All Rights Reserved
+         </div>
+      </footer>
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+   </body>
+</html>
